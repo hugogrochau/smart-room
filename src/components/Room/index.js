2b71646646eb25c9c27399c315dac39ad1a422c3
Hugo Grochau <hugo@nuuvem.com>
2017-05-22 12:02:06 -0300

    Add heatmap

--- src/components/Room/index.js
+++ src/components/Room/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import simpleheat from 'simpleheat';
 import Sensor from '../Sensor';
 
 import positionMap from './positionMap';
@@ -23,14 +23,35 @@ class Room extends Component {
     };
   }
 
+  drawHeatMap = () => {
+    this.heatMap.clear();
+    this.props.sensors.forEach(sensor => {
+      const positions = positionMap[sensor.position];
+      this.heatMap.add([(positions.left + 320) / 1.9, (positions.top + 400) / 5, sensor.temperature]);
+    });
+    this.heatMap.radius(30, 100);
+    this.heatMap.draw();
+  }
+
+  componentDidMount() {
+    this.heatMap = simpleheat(this.canvas);
+    this.drawHeatMap();
+  }
+
+  componentDidUpdate() {
+    this.drawHeatMap();
+  }
+  
+  
   render() {
     const { sensors } = this.props;
 
     return (
       <div className="Room" style={{backgroundImage: `url(${roomImage})`}}>
-          {sensors.map(sensor => 
-            <Sensor { ...sensor } { ...this.sensorStyleByPosition(sensor.position) }  key={sensor.id} />
-          )}
+        <canvas ref={(canvas) => this.canvas = canvas} className="HeatMap" />
+        {sensors.map(sensor => 
+          <Sensor { ...sensor } { ...this.sensorStyleByPosition(sensor.position) }  key={sensor.id} />
+        )}
       </div>
     );
   }
